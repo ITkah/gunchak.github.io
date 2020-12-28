d115bdd7046339d4a07d1d11627f2705170aefe1
ITkah <42772919+ITkah@users.noreply.github.com>
2020-12-28 22:33:14 +0200

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,42 @@
+$('.popup-with-zoom-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+});
+
+
+
+$(".call_timer").on("click", function(e) {
+    e.preventDefault();
+    $(".timer_back").show();
+    var timer2 = "1:00";
+    var interval = setInterval(function() {
+        var timer = timer2.split(':');
+        //by parsing integer, I avoid all extra string processing
+        var minutes = parseInt(timer[0], 10);
+        var seconds = parseInt(timer[1], 10);
+        --seconds;
+        minutes = (seconds < 0) ? --minutes : minutes;
+        if (minutes < 0) clearInterval(interval);
+        seconds = (seconds < 0) ? 59 : seconds;
+        seconds = (seconds < 10) ? '0' + seconds : seconds;
+        //minutes = (minutes < 10) ?  minutes : minutes;
+        $('.countdown').html(minutes + ':' + seconds);
+        timer2 = minutes + ':' + seconds;
+    }, 1000);
+
+    function closeTimer() {
+        $(".timer_back").hide();
+    }
+    setTimeout(closeTimer, 60000);
+});
